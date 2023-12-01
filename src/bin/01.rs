24317df9053c8302c59649ded4c2a8be022bc8cb
SkyfallWasTaken <mahadkalam1@proton.me>
2023-12-01 21:59:09 +0000

    Bad solution to Day 1 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -19,7 +19,34 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut total = 0;
+
+    for line in input.lines() {
+        let line = line
+            .replace("one", "o1ne")
+            .replace("two", "t2o")
+            .replace("three", "th3ee")
+            .replace("four", "fo4r")
+            .replace("five", "fi5e")
+            .replace("six", "s6x")
+            .replace("seven", "se7en")
+            .replace("eight", "eig8t")
+            .replace("nine", "ni9e");
+
+        let num_iter: Vec<&str> = line
+            .match_indices(|r#char| char::is_numeric(r#char))
+            .map(|(_, n)| n)
+            .collect();
+        let first = num_iter.first().unwrap();
+        let last = num_iter.last().unwrap();
+        let num = (first.to_string() + last).parse::<u32>().unwrap();
+
+        // dbg!(first, last, num, &line);
+
+        total += num;
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -35,7 +62,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        todo!();
-        // assert_eq!(result, None);
+        assert_eq!(result, Some(281));
     }
 }
